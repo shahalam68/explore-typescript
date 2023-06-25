6801681c758e33649699dfde5192272e33a217bb
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-06-25 10:24:38 +0600

    array

---
+++ array.ts
@@ -0,0 +1,8 @@
+const salary:number = 7500;
+const friendSalaries:number[] = [7500, 12300,17200,9400];
+const friends: string[] = ['soykot', 'tamim',"musfiq"];
+
+friendSalaries[0] = 10500;
+friendSalaries.push(13500);
+friendSalaries[4] = ' 9480';
+friends.push('masrafi')
--- first.ts
+++ first.ts
@@ -1,3 +1,25 @@
-let money = 5000;
-money = '';
-money = false;
+let money:number = 5;
+let honey:string = 'Hi baby';
+let funny: boolean = true;
+const age:number = 21;
+
+
+
+function add(first:number, second:number):number{
+    const result = first + second;
+    return result;  
+}
+
+
+// multipurpose
+function add2(first:number | string, second:number | string):string{
+    const result = first + second;
+    return result + '';  
+}
+
+function doubleConsole ( number:number){
+    console.log(2*number);
+}
+
+const output:number = add(45,37);
+const fullName = add2('adam','send');
