4a197f507dcc37d23908fae7c3cea83b72f844cb
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-06-26 15:05:44 +0600

    class

---
+++ class.ts
@@ -0,0 +1,17 @@
+class Car{
+    model:string;
+    price: number;
+    constructor(model: string, price: number){
+        this.model = model;
+        this.price = price
+    }
+
+    getTotalPrice(tax:number):number{
+        const taxAmount = this.price * tax/100;
+        const total = this.price + taxAmount;
+        return total;
+    }
+}
+
+const toyota = new Car('toyota', 100000);
+const totalPrice:number = toyota.getTotalPrice(25);
