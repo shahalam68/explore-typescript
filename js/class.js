7639c67d295148218403a382149f5a089f36cf7e
“shahalam68” <a.k.mshahalam68@gmail.com>
2024-01-17 23:17:49 +0600

    test

--- js/class.js
+++ js/class.js
@@ -9,11 +9,11 @@ class Car {
         const realMillage = this._millage + 50000;
         return realMillage;
     }
-    getTotalPrice(tax) {
+    getTotalPrice(tax) { 
         const taxAmount = this.price * tax / 100;
         const total = this.price + taxAmount;
         return total;
     }
 }
-const toyota = new Car('toyota', 100000);
+const toyota =  new Car('toyota', 100000);
 const totalPrice = toyota.getTotalPrice(25);
